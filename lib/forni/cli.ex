fa8d652d4526b164d40d20267dde59c2adea3834
Nicolás Riquelme <nrriquelme@gmail.com>
2019-09-02 00:04:20 -0400

    Parsing data

--- lib/forni/cli.ex
+++ lib/forni/cli.ex
@@ -1,2 +1,16 @@
 defmodule Forni.Cli do
+  def main(argv) do
+    OptionParser.parse(argv, switches: [create: :boolean, migrate: :boolean])
+    |> parse_args
+  end
+
+  def parse_args({value, _, _}) do
+    [type_of_key] = Keyword.keys(value)
+
+    case type_of_key do
+      type_of_key when type_of_key == :create -> IO.puts("Creation migration")
+      type_of_key when type_of_key == :migrate -> IO.puts("Migraiton!!!")
+      _ -> IO.puts("No option")
+    end
+  end
 end
